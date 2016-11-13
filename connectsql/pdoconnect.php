a2b6dc946dcfdaf5a1e1ce5e66a146ec5b385885
Idriss <kyworn@gmail.com>
2016-11-13 11:05:57 +0100

    Mise à jour global

--- connectsql/pdoconnect.php
+++ connectsql/pdoconnect.php
@@ -1,8 +1,8 @@
 <?php
-$host = "localhost";
-$user = "admin";
-$password = "admin";
-$database_name = "blogname;charset=utf8";
+$host = "mysql.hostinger.fr";
+$user = "u625628409_cms";
+$password = "idriss";
+$database_name = "u625628409_cms;charset=utf8";
 $pdo = new PDO("mysql:host=$host;dbname=$database_name", $user, $password, array(
 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
 ));
